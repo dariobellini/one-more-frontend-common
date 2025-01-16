49a6e0837502e89a187380c1a978c1386d7b070c
Miliar <dario.bellini3@gmail.com>
2025-01-16 20:01:26 +0100

    Update Constants.ts

--- projects/one-more-fe-service/src/Constants.ts
+++ projects/one-more-fe-service/src/Constants.ts
@@ -1,6 +1,10 @@
 // const BASE_PATH: string = "http://192.168.8.130:7253";
 // const BASE_PATH: string = "http://192.168.1.56:7253";
 const BASE_PATH: string = "http://192.168.1.60:7253";
+
+//Decommentare per Debug:
+//const BASE_PATH: string = "http://localhost:5000";
+
 // const BASE_PATH: string = "http://199.247.15.22";
 //const BASE_PATH: string = "https://one-more-service-api.it/api";
 const USER_API_JWT : string = "user-api-jwt";
