12f95874a2a5d5bae9484f3c107a5d158c3c14ba
marco cangi <marcocangi@hotmail.it>
2025-01-24 00:10:19 +0100

    fix favorites

--- projects/one-more-fe-service/src/user-service.ts
+++ projects/one-more-fe-service/src/user-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Constants } from './Constants';
-
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +12,23 @@ export class UserService {
   constructor(private http:HttpClient, 
               private constants:Constants) { }
 
-  AddRemoveFavorite(idSoggetto: number, idAttivita: number): Observable<boolean>{
-
+  async AddRemoveFavorite(idSoggetto: number, idAttivita: number): Promise<boolean> {
     const params = new HttpParams()
-    .set('idSoggetto', idSoggetto.toString())
-    .set('idAttivita', idAttivita.toString());
-
-    return this.http.get<boolean>(this.constants.BasePath()+'/User/AddRemoveFavorite',{params});
+      .set('idSoggetto', idSoggetto.toString())
+      .set('idAttivita', idAttivita.toString());
+  
+    try {
+      // Usa firstValueFrom per convertire l'observable in una Promise
+      const result = await firstValueFrom(
+        this.http.get<boolean>(this.constants.BasePath() + '/User/AddRemoveFavorite', { params })
+      );
+      return result;
+    } catch (error) {
+      console.error('Errore durante la chiamata HTTP:', error);
+      throw error; // Puoi gestire diversamente l'errore se necessario
+    }
   }
+  
   apiCheckIsFavorite(idSoggetto: number, idAttivita: number): Observable<boolean> {
     const params = new HttpParams()
     .set('idSoggetto', idSoggetto.toString())
