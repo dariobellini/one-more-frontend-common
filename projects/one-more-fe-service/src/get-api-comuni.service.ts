c1f34f013aba9dada6e5001ae32d39791dd8b97c
FlavioIsidori <flavio.isidori@outlook.com>
2025-05-14 21:21:15 +0200

    aggiunta del metodo setlistaComuni

--- projects/one-more-fe-service/src/get-api-comuni.service.ts
+++ projects/one-more-fe-service/src/get-api-comuni.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Comuni } from './EntityInterface/Comuni_CAP';
 import { Constants } from './Constants';
 import { AuthService } from './Auth/auth.service';
@@ -10,6 +10,7 @@ import { AuthService } from './Auth/auth.service';
 })
 export class GetApiComuniService {
   language : string | undefined;
+  private listaComuniSubject = new BehaviorSubject<Comuni[] | null>(null);
 
   constructor(private http:HttpClient, private constants: Constants,
     private authService: AuthService) { }
@@ -22,4 +23,18 @@ export class GetApiComuniService {
       }
    });
   }
+   setlistaComuni(listaComuni:Comuni[]) {
+    listaComuni.sort((a, b) => {
+        const descrizioneA = a.descComune.toLowerCase();
+        const descrizioneB = b.descComune.toLowerCase();
+        if (descrizioneA < descrizioneB) {
+          return -1;
+        }
+        if (descrizioneA > descrizioneB) {
+          return 1;
+        }
+        return 0;
+      });
+      this.listaComuniSubject.next(listaComuni);
+    }
 }
