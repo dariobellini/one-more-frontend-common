b4cfac5fea72d2df4a7f28239043664efa26fbb8
marco cangi <marcocangi@hotmail.it>
2025-07-20 10:51:14 +0200

    fix 5.0

--- projects/one-more-fe-service/src/search-api.service.ts
+++ projects/one-more-fe-service/src/search-api.service.ts
@@ -11,9 +11,10 @@ import { Observable } from 'rxjs';
 export class SearchApiService {
   constructor(private http:HttpClient, private constants:Constants) { }
 
-  async Search(input:string): Promise<Observable<SearchItemDto[]>> {
-    return this.http.get<SearchItemDto[]>(this.constants.BasePath() + '/search/aux-search?input='+input);
-  }
+  async Search(input: string): Promise<Observable<SearchItemDto[]>> {
+  return this.http.get<SearchItemDto[]>(this.constants.BasePath() + '/search/aux-search?input=' + input);
+}
+
 
 }
 
