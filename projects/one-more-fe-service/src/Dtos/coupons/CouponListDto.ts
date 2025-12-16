687eb6a17fcc32222f36fb0d37812ca40d93eaf1
Miliar <dario.bellini3@gmail.com>
2025-12-16 12:53:40 +0100

    Add info to coupon-list

--- projects/one-more-fe-service/src/Dtos/coupons/CouponListDto.ts
+++ projects/one-more-fe-service/src/Dtos/coupons/CouponListDto.ts
@@ -1,14 +1,13 @@
-import { PromoListDto } from "../PromoListDto";
 import { CouponDto } from "./CouponDto";
 
-export class CouponListDto {
-    coupon!: CouponDto;
-    promo!: PromoListDto;
+export class CouponListDto extends CouponDto {
+    shopId!: number;
+    shopTitle!: string;
+    promoTitle!: string;
+    photoFileName?: string;
+    promoEndDate?: Date;
 
     constructor(data?: Partial<CouponListDto>) {
-        if (data) {
-            this.coupon = data.coupon!;
-            this.promo = data.promo!;
-        }
+        super(data);
     }
 }
