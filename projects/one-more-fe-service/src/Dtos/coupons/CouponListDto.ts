ad52b35694b54a235ce12afddd91f54fb7119009
FlavioIsidori <flavio.isidori@outlook.com>
2025-12-17 23:11:54 +0100

    modifica oggetto CouponListDto per far vedere i coupon lato utente

--- projects/one-more-fe-service/src/Dtos/coupons/CouponListDto.ts
+++ projects/one-more-fe-service/src/Dtos/coupons/CouponListDto.ts
@@ -1,13 +1,23 @@
+import { PromoListDto } from "../PromoListDto";
 import { CouponDto } from "./CouponDto";
 
-export class CouponListDto extends CouponDto {
-    shopId!: number;
-    shopTitle!: string;
-    promoTitle!: string;
-    photoFileName?: string;
-    promoEndDate?: Date;
+export class CouponListDto
+//  extends CouponDto
+  {
+    // shopId!: number;
+    // shopTitle!: string;
+    // promoTitle!: string;
+    // photoFileName?: string;
+    // promoEndDate?: Date;
+    coupon!: CouponDto;
+    promo!: PromoListDto;
+
 
     constructor(data?: Partial<CouponListDto>) {
-        super(data);
+        // super(data);
+        if(data){
+        this.coupon = data.coupon!;
+        this.promo = data.promo!;
+        }
     }
 }
