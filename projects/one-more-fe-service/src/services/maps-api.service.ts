f52e3660a0007ed0134780c85e03a2ec365223d8
marco cangi <marcocangi@hotmail.it>
2026-02-12 19:59:47 +0100

    fix cerca qui map

--- projects/one-more-fe-service/src/services/maps-api.service.ts
+++ projects/one-more-fe-service/src/services/maps-api.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Constants } from '../Constants';
 import { MapResDto } from '../Dtos/Responses/map/MapResDto';
@@ -8,16 +8,28 @@ import { MapResDto } from '../Dtos/Responses/map/MapResDto';
   providedIn: 'root'
 })
 export class MapsApiService {
-  constructor(private http: HttpClient, private constants: Constants) {}
 
-  /**
-   * Recupera la lista degli shop per la mappa
-   * @param latitude Latitudine della posizione corrente
-   * @param longitude Longitudine della posizione corrente
-   */
+  http = inject(HttpClient);
+  constants = inject(Constants);
+
+  constructor() {}
+
   Get(latitude: number, longitude: number): Observable<MapResDto> {
     return this.http.get<MapResDto>(
       `${this.constants.BasePath()}/maps/get?latitude=${latitude}&longitude=${longitude}`
     );
+  }
+
+  GetInBounds(swLat: number, swLng: number, neLat: number, neLng: number): Observable<MapResDto> {
+    const params = {
+      latitude: '0', // non usato quando ci sono i bounds, ma il param è richiesto
+      longitude: '0',
+      swLat: swLat.toString(),
+      swLng: swLng.toString(),
+      neLat: neLat.toString(),
+      neLng: neLng.toString()
+    };
+
+    return this.http.get<MapResDto>(`${this.constants.BasePath()}/maps/get-in-bounds`, { params });
   }
 }
